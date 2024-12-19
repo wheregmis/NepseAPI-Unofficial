226a3aa240611f07f1755bd1c26edb8874c7d1d9
Suraj Rimal <davidparkedme@gmail.com>
2024-12-19 14:24:23 +0545

    fixes

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 1. Create and activate virtual environment
+if [ -d "venv" ]; then
+    echo "Removing existing virtual environment..."
+    rm -rf venv
+fi
+
+echo "Creating new virtual environment..."
+python3 -m venv venv
+source venv/bin/activate
+
+# 2. Upgrade pip
+python -m pip install --upgrade pip
+
+# 3. Install dependencies
+pip install -r requirements.txt
+
+echo "Setup complete!"
